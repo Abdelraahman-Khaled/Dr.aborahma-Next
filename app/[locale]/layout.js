d85679e83d06a0a16880ce333c4965b3684c278e
Abdelrahman Khaled <109894754+Abdelraahman-Khaled@users.noreply.github.com>
2026-03-05 17:19:36 +0200

    update

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -54,21 +54,30 @@ export default async function RootLayout({ children, params }) {
 
         <Script src="/js/jquery-3.7.1.min.js" strategy="beforeInteractive" />
         <Script src="/js/bootstrap.min.js" strategy="beforeInteractive" />
-        <Script src="/js/validator.min.js" strategy="lazyOnload" />
-        <Script src="/js/jquery.waypoints.min.js" strategy="lazyOnload" />
-        <Script src="/js/jquery.counterup.min.js" strategy="beforeInteractive" />
+        <Script src="/js/validator.min.js" strategy="afterInteractive" />
+        <Script src="/js/jquery.waypoints.min.js" strategy="afterInteractive" />
+        <Script
+          src="/js/jquery.counterup.min.js"
+          strategy="beforeInteractive"
+        />
         <Script src="/js/swiper-bundle.min.js" strategy="beforeInteractive" />
-        <Script src="/js/jquery.magnific-popup.min.js" strategy="lazyOnload" />
-        <Script src="/js/SmoothScroll.js" strategy="lazyOnload" />
-        <Script src="/js/parallaxie.js" strategy="lazyOnload" />
-        <Script src="/js/gsap.min.js" strategy="lazyOnload" />
-        <Script src="/js/magiccursor.js" strategy="lazyOnload" />
-        <Script src="/js/SplitText.js" strategy="lazyOnload" />
-        <Script src="/js/ScrollTrigger.min.js" strategy="lazyOnload" />
-        <Script src="/js/jquery.mb.YTPlayer.min.js" strategy="lazyOnload" />
-        <Script src="/js/wow.min.js" strategy="lazyOnload" />
-        <Script src="/js/jquery.slicknav.js" strategy="lazyOnload" />
-        <Script src="/js/function.js" strategy="lazyOnload" />
+        <Script
+          src="/js/jquery.magnific-popup.min.js"
+          strategy="afterInteractive"
+        />
+        <Script src="/js/SmoothScroll.js" strategy="afterInteractive" />
+        <Script src="/js/parallaxie.js" strategy="afterInteractive" />
+        <Script src="/js/gsap.min.js" strategy="afterInteractive" />
+        <Script src="/js/magiccursor.js" strategy="afterInteractive" />
+        <Script src="/js/SplitText.js" strategy="afterInteractive" />
+        <Script src="/js/ScrollTrigger.min.js" strategy="afterInteractive" />
+        <Script
+          src="/js/jquery.mb.YTPlayer.min.js"
+          strategy="afterInteractive"
+        />
+        <Script src="/js/wow.min.js" strategy="afterInteractive" />
+        <Script src="/js/jquery.slicknav.js" strategy="afterInteractive" />
+        <Script src="/js/function.js" strategy="afterInteractive" />
       </body>
     </html>
   );
